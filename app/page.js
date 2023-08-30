f05451a6209e25d8a581655ad24a3968a0c0308c
Hielke <33683530+HielkeGugten@users.noreply.github.com>
2023-08-30 17:19:22 +0200

    Worked all day on this instead of work hehehehe

--- app/page.js
+++ app/page.js
@@ -1,113 +1,194 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import React from "react";
+import dynamic from "next/dynamic";
+import { Link } from "react-scroll";
+import Image from "next/image";
+
+import Lightning from "../public/lightning.svg";
+import Cloud from "../public/cloud.svg";
+import Arrow from "../public/arrow.svg";
+import ArrowBlack from "../public/arrow_black.svg";
+import CloudBlack from "../public/cloud_black.svg";
+import Cloud2 from "../public/cloud2.svg";
+import LightningBlack from "../public/lightning_black.svg";
+import Smiley from "../public/smiley.svg";
+import Star from "../public/star.svg";
+import Stars from "../public/stars.svg";
+
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
+
+export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
+    <main className="flex relative min-h-screen flex-col items-center justify-between mx-auto overflow-x-hidden">
+      <div className="h-screen flex flex-col items-center justify-center">
+        <h1
+          className="mt-1 text-black animate-bounce font-dynapuff font-bold"
+          style={{ fontSize: "calc(11vw)" }}
+        >
+          Joyce Turns 26!
+        </h1>
+        <ul>
+          <li className="inline m-2 text-lg hover:text-white cursor-pointer">
+            <Link
+              to="video-section" // ID of the target section
+              smooth={true} // Enable smooth scrolling
+              duration={500} // Duration of the scroll animation in milliseconds
+            >
+              Video
+            </Link>
+          </li>
+          <li className="inline m-2 text-lg hover:text-white cursor-pointer">
+            <Link
+              to="gallery-section" // ID of the target section
+              smooth={true} // Enable smooth scrolling
+              duration={500} // Duration of the scroll animation in milliseconds
+            >
+              Gallery
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="flex relative flex-col py-10 max-w-5xl p-10">
+        <Lightning className="rotate-[17deg] absolute z-0 right-[-200px] -top-10" />
+        <Cloud className="rotate-[-25deg] absolute z-0 -left-52" />
+        <Arrow className="rotate-[7deg] absolute z-0 top-52 -left-35" />
+
+        <h1 className="text-[40px] font-dynapuff font-bold float-left w-full mb-10 z-10">
+          Joyce’s Fabulous 26th
+        </h1>
+        <div className="flex z-10">
+          <p className="text-white text-[20px] mr-4">
+            Get ready for an explosion of Purple, Fun, and Love! Our lovely
+            Joyce is turning 26, and we’ve organized this sensational website to
+            celebrate her special day.
+          </p>
+          <p className="text-white text-[20px]">
+            Browse through heart-warming memories, enjoy Hielke’s touching video
+            message, and be a part of this fantastic virtual birthday party.
+            Let’s make Joyce’s 26th a day to remember!
+          </p>
+        </div>
+      </div>
+      <section id="video-section" className="h-screen py-20 max-w-5xl p-10">
+        <h1 className="text-[40px] font-dynapuff font-bold text-center w-full mb-10">
+          Video
+        </h1>
+        <p className="text-white text-[20px] mb-10">
+          Just for this occassion, Hielke contacted his friends from Africa,
+          and had them record a Happy Birthday video for Joyce!
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+
+        <div className="player-wrapper relative pt-[56.25%]">
+        <ReactPlayer
+        className="absolute top-0 left-0"
+          width='100%'
+          height='100%'
+          url="https://www.youtube.com/watch?v=wWgIAphfn2U"
+          controls
+          loop
+        />
+        </div>
+      </section>
+      <div className="flex relative">
+        <CloudBlack className="rotate-[20deg] z-0 scale-75" />
+        <LightningBlack className="rotate-[-10deg] z-0 scale-75" />
+        <ArrowBlack className="rotate-[-10deg] z-0 scale-75" />
+        <CloudBlack className="rotate-[-5deg] z-0 scale-75" />
+        <Smiley className="rotate-[-10deg] z-0 scale-75" />
+        <Stars className="rotate-[20deg] z-0 scale-75" />
+      </div>
+      <section id="gallery-section" className="h-screen flex flex-col relative items-center max-w-5xl p-10 mb-40">
+          <h1 className="text-[40px] font-dynapuff font-bold text-center mb-10">
+            Gallery
+          </h1>
+          <p className="text-white text-[20px] m-5">
+            Over the years Joyce has been very busy! That&apos;s why we provide
+            a gallery to fully appreciate her face!
+          </p>
+        <div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/pics/1.jpg"
+            data-fancybox="gallery"
+            data-caption="Caption #1"
           >
-            By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/pics/1.jpg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </a>
+          <a
+            href="/pics/2.jpg"
+            data-fancybox="gallery"
+            data-caption="Caption #2"
+          >
+            <Image
+              src="/pics/2.jpg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </a>
+          <a
+            href="/pics/3.jpg"
+            data-fancybox="gallery"
+            data-caption="Caption #3"
+          >
+            <Image
+              src="/pics/3.jpg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </a>
+          <a
+            href="/pics/1.jpg"
+            data-fancybox="gallery"
+            data-caption="Caption #4"
+          >
+            <Image
+              src="/pics/1.jpg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </a>
+          <a
+            href="/pics/2.jpg"
+            data-fancybox="gallery"
+            data-caption="Caption #5"
+          >
+            <Image
+              src="/pics/2.jpg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </a>
+          <a
+            href="/pics/3.jpg"
+            data-fancybox="gallery"
+            data-caption="Caption #6"
+          >
+            <Image
+              src="/pics/3.jpg"
+              width={500}
+              height={500}
+              alt="Picture of the author"
             />
           </a>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      <div class="p-10 text-lg text-white absolute bottom-0">Website created with ❤️ by Hielke © 2023</div>
     </main>
-  )
+  );
 }
